6dff381f573f012fe4a4e5a1899033faffa1e399
Ryan Busby <BusbyActual@gmail.com>
2017-12-04 20:18:12 -0800

    Added css and component heirarchy

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import NavComponent from './components/nav.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavComponent/>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
